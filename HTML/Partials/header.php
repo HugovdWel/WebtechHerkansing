ad73271c725ec17ff64b4e952d8bb2d3a281e7f9
Hugo <hugo00.nogwat@gmail.com>
2019-07-12 15:20:13 +0200

    Soup update

--- HTML/Partials/header.php
+++ HTML/Partials/header.php
@@ -1,16 +1,14 @@
 <link rel="stylesheet" href="../CSS/header.css">
 <link rel="stylesheet" href="../CSS/flex.css">
 
-<div class="header standardColors flex_Box flex_justify-stretch">
-    <div class="flex_item">
-        <h1>
-            ARGGGGGGGGGGGGGG
-    	</h1>
+<div class="header standardColors flex_box flex_justify-stretch">
+    <div class="flex_item logo header-item">
+        
     </div>
-    <div class="flex_item">
-        woord
+    <div class="flex_item header-item">
+        <h1>EendenVrienden.nl</h1>
     </div>
-    <div class="flex_item">
+    <div class="flex_item header-item">
         nee
     </div>
 </div>
