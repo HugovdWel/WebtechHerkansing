d6f873e9e369cc8e693b7a9e9281327492961596
Hugo <hugo00.nogwat@gmail.com>
2019-07-13 18:02:57 +0200

    Soup update

--- HTML/Partials/footer.php
+++ HTML/Partials/footer.php
@@ -1,10 +1,9 @@
 <link rel="stylesheet" href="../CSS/footer.css">
-<div class="footer standardColors flex_box flex_justify-stretch">
+<div class="footer standardStyle flex_box flex_justify-stretch">
     <div class="flex_item">
         Heb je klachten? stop ze in je gat u bitch.
     </div>
     <div class="flex_item">
-        <h1></h1>
     </div>
     <div class="flex_item">
         Contact informatie: Himalaya baan 659872
---
+++ HTML/Partials/navbar.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../CSS/navbar.css">
+<div class="navbar-style flex_box flex_justify-stretch">
+    <?php
+    foreach 
+    ?>
+    <div class="flex_item">
+        
+    </div>
+</div>
