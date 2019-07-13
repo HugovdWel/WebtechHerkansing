0a0781a71bccaa7b9286c4a5bf014e5ad9cd27f9
Hugo <hugo00.nogwat@gmail.com>
2019-07-13 17:00:45 +0200

    Footer.

---
+++ HTML/Partials/footer.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="../CSS/footer.css">
+<div class="footer standardColors flex_box flex_justify-stretch">
+    <div class="flex_item">
+        Heb je klachten? stop ze in je gat u bitch.
+    </div>
+    <div class="flex_item">
+        <h1></h1>
+    </div>
+    <div class="flex_item">
+        Contact informatie: Himalaya baan 659872
+    </div>
+</div>
