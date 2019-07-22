7593df3fd44ac1c83e7d55d369425d683169afd3
Hugo <hugo00.nogwat@gmail.com>
2019-07-22 15:41:52 +0200

    footer update

--- HTML/Partials/footer.php
+++ HTML/Partials/footer.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="../CSS/footer.css">
 <div class="footer standardStyle flex_box flex_justify-stretch">
-    <div class="flex_item">
+    <div class="flex_item addPadding">
         Heb je klachten? stop ze in je gat u bitch.
     </div>
-    <div class="flex_item">
+    <div class="flex_item addPadding">
     </div>
-    <div class="flex_item">
+    <div class="flex_item addPadding">
         Contact informatie: Himalaya baan 659872
     </div>
 </div>
