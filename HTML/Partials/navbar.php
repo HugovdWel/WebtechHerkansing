41ebaa37beb470adc5a41a34e07968f52c72598a
Hugo <hugo00.nogwat@gmail.com>
2019-07-22 09:41:45 +0200

    Soup update

--- HTML/Partials/navbar.php
+++ HTML/Partials/navbar.php
@@ -1,9 +1,30 @@
 <link rel="stylesheet" href="../CSS/navbar.css">
-<div class="navbar-style flex_box flex_justify-stretch">
-    <?php
-    foreach 
-    ?>
-    <div class="flex_item">
-        
+<div class="navbar-style alternativeStyle flex_box flex_justify-center">
+    
+    <a href="" class="flex_item addPadding navbar-item-style">
+    <div class=" navbar buttonStyle">
+         test 
     </div>
+    </a>
+    <a href="" class="flex_item addPadding navbar-item-style">
+    <div class="navbar buttonStyle">
+         test 
+    </div>
+    </a>
+    <a href="" class="flex_item addPadding navbar-item-style">
+    <div class="navbar buttonStyle">
+         test 
+    </div>
+    </a>
+    <a href="" class="flex_item addPadding navbar-item-style">
+    <div class="navbar buttonStyle">
+         test 
+    </div>
+    </a>
+    <a href="" class="flex_item addPadding navbar-item-style">
+    <div class="navbar buttonStyle">
+         test 
+    </div>
+    </a>
+
 </div>
