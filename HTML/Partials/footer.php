8d3ffb6a809864ffb24d1a8dcd763978c1bef1fc
Hugo <hugo00.nogwat@gmail.com>
2019-07-22 16:16:12 +0200

    Shits still fucked, you'll live

--- HTML/Partials/footer.php
+++ HTML/Partials/footer.php
@@ -1,11 +1,12 @@
 <link rel="stylesheet" href="../../CSS/footer.css">
 <div class="footer standardStyle flex_box flex_justify-stretch">
-    <div class="flex_item addPadding">
+    
+    <div class="flex_item addPadding footer-item">
         Heb je klachten? stop ze in je gat u bitch.
     </div>
-    <div class="flex_item addPadding">
+    <div class="flex_item addPadding footer-item">
     </div>
-    <div class="flex_item addPadding">
+    <div class="flex_item addPadding footer-item">
         Contact informatie: Himalaya baan 659872
     </div>
 </div>
