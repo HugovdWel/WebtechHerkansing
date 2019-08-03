4819e8645251087e6e58a7deab7f6671f377fe02
Daniël <dj.vervloed@gmail.com>
2019-08-03 15:13:04 +0200

    8

--- HTML/Pages/Homepage.php
+++ HTML/Pages/Homepage.php
@@ -8,8 +8,8 @@ include '../Partials/pageEnd.php';
 ?>
 <body>
 
-<div class ="flex_box flex_justify-stretch textAlignCenter">
-    <section>
+<div class ="flex_box flex_justify-center textAlignCenter">
+    <section class="flex_item">
 
         <h2>Waarom deze webpagina bestaat</h2>
         <p>Deze pagina bestaat om u alle informatie die u mogenlijk zou kunnen willen hebben over eenden.</p>
@@ -17,7 +17,7 @@ include '../Partials/pageEnd.php';
         <p>Het meest voorkomende gebruik van de "eend" is het dier: de Eend. Hieronder een citaat van Wikipedia over de Eend</p>
     </section>
 
-    <section><h2>De Eend</h2>Eend is de algemene naam voor een aantal soorten vogels uit de familie van eendachtigen (Anatidae). Alle soorten uit de Anatidae worden "eenden" genoemd, behalve de soorten uit de onderfamilie Anserinae, de ganzen en zwanen. Eenden zijn hoofdzakelijk aquatische vogels, meestal kleiner dan hun verwanten, de zwanen en de ganzen, met een kortere nek, en kunnen in zowel zoet als zout water worden gevonden. De Tadorninae, waartoe onder andere de bergeend behoort, houden qua formaat het midden tussen een gans en een eend.
+    <section class="flex_item"><h2>De Eend</h2>Eend is de algemene naam voor een aantal soorten vogels uit de familie van eendachtigen (Anatidae). Alle soorten uit de Anatidae worden "eenden" genoemd, behalve de soorten uit de onderfamilie Anserinae, de ganzen en zwanen. Eenden zijn hoofdzakelijk aquatische vogels, meestal kleiner dan hun verwanten, de zwanen en de ganzen, met een kortere nek, en kunnen in zowel zoet als zout water worden gevonden. De Tadorninae, waartoe onder andere de bergeend behoort, houden qua formaat het midden tussen een gans en een eend.
         Eenden worden soms verward met verscheidene soorten niet verwante vogels met gelijkaardige vormen, zoals duikers, futen, rallen, koeten en waterhoentjes. Soms wordt met "eend" alleen vrouwtjeseenden bedoeld. Een mannetjeseend heet een "woerd". Een jonge eend wordt pijltje (soms piel) genoemd. In het algemeen worden jongen van kippen en eenden ook wel "pulletje" of "pulleke" genoemd. Een woerd van de wilde eend, de meest voorkomende soort, is naast de kleur ook te herkennen aan enkele gekrulde veren op de staart (dit is niet altijd zichtbaar).
     </section><br>
 </div>
