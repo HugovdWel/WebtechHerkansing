9c3a1190ddbadaf0369c256074d5d45e2021ebe9
Hugo <hugo00.nogwat@gmail.com>
2019-08-04 15:07:07 +0200

    Dus

--- PHP/databaseConnection.php
+++ PHP/databaseConnection.php
@@ -1,23 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$dbhost     = "localhost";
+$dbname     = "Eendenvrienden";
+$dbuser     = "root";
+$dbpass     = "";
+
                                                                                 echo" Ik wordt geladen ";
 
 createDatabaseConnection();
 
 function createDatabaseConnection(){
 
-    global $servername, $username, $password;
+    global $dbhost, $dbname, $dbuser, $dbpass;
                                                                                 echo" Ik run ";
     try{
-        $server = new mysqli($servername, $username, $password);
+        $connection = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
     } catch(PDOExeption $e){
         throw new \PDOException($e->getMessage(), (int)$e->getCode());
                                                                                 echo" Ik crash ";
     }
 }
 
+// $quary = sprintf("select * from forumpost");
+// $result = mysql_query($query);
+var_dump($result);
+// echo $result[0];
 
 
 ?>
