c6ee54ea910ee10fd4ed147a4146b6eaf04d6072
Hugo <hugo00.nogwat@gmail.com>
2019-09-03 23:07:37 +0200

    Wijn update, omdat ik zo fancy ben

--- PHP/databaseConnection.php
+++ PHP/databaseConnection.php
@@ -1,25 +1,35 @@
 <?php
-$dbhost     = "(local)";
-$dbname     = "eendenvrienden";
-$dbuser     = "databaseConnection";
-$dbpass     = "4fpsFTW";
 
 createDatabaseConnection();
+haalGebruikersData(1);
 
 function createDatabaseConnection(){
 
-    global $dbhost, $dbname, $dbuser, $dbpass;
+    $dbhost     = "(local)";
+    $dbname     = "eendenvrienden";
+    $dbuser     = "databaseConnection";
+    $dbpass     = "4fpsFTW";
 
+    global $connection;
     $connection = new PDO("sqlsrv:Server=$dbhost;Database=$dbname;ConnectionPooling=0","$dbuser","$dbpass");
+}
 
+function haalGebruikersData($gebruikersId){
+    global $connection;
     $sql = ("select * from Users");
     $preparedQuary = $connection->prepare($sql);
 
     $preparedQuary->execute();
-    $data = $preparedQuary->fetch();
+    $data = $preparedQuary->fetchAll();
     var_dump($data);
     echo("<br></br>");
-    echo($data[1]);
+    echo($data[0][0]);
+    echo("<br></br>");
+    echo($data[1][0]);
+}
+
+function haalPersoonsGegevens($gebruikersId){
+
 }
 
 
