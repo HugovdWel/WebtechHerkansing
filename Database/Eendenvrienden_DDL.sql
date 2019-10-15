dedc2725b4f6a17e63b6f834d97cc725c05ab4eb
Hugo <hugo00.nogwat@gmail.com>
2019-10-15 14:21:14 +0200

    goed genoeg

--- Database/Eendenvrienden_DDL.sql
+++ Database/Eendenvrienden_DDL.sql
@@ -1,11 +1,15 @@
 use master
+GO
 drop database eendenvrienden
+GO
 create database eendenvrienden
+GO
 use eendenvrienden
+GO
 
 create table Users(
 username	varchar(50)		unique not null,
-[password]	varchar(50)		not null,
+[password]	varchar(100)		not null,
 email		varchar(50)		not null,
 [user_id]	numeric(12)		not null identity(1,1)
 constraint PK_USERS primary key ([user_id])
@@ -70,12 +74,12 @@ constraint FK_COMMENTS_POST_ID FOREIGN KEY (post_id) REFERENCES ForumPost (post_
 		on delete cascade
 		on update cascade
 )
-
+GO
 
 -----------------------------------------------------------------------------------------
 insert into Users(username, [password], email)
-values  ('hugo','qwerty123', 'nvt'),
-		('daniël','qwerty1234', 'nvt')
+values  ('hugo','$2y$10$AChoygtdi.Esgck8KDxFnOjy5YqcsdIXQm03BrG6QP9.okhng9YsW', 'hugo00.nogwat@gmail.com'), /*qwerty123*/
+		('daniël','$2y$10$MnwEtE/YbW.tTVft3AkQFellltTfhbQhXErmnDEtI9P/1CNBDwlo2', 'dj.vervloed@gmail.com') /*qwerty1234*/
 insert into Picture([user_id], [name], [file_name])
 values	(1, 'cute', 'lol.jpg'),
 		(2, 'fun', 'jk.jpg')
@@ -83,12 +87,12 @@ values	(1, 'cute', 'lol.jpg'),
 insert into Video([user_id], [name], link, [description])
 values  (1, 'Schattig eenden', 'www.koekje.nl', 'moet je kijken'),
 		(2, 'Beste eend', 'www.biscuit.nl', 'moet je kijken!')
-
-insert into Recipes
+		/*
+insert into Recipes([user_id], [name], picture_id, [description])
 values  (1, 'gefrituurde eend', '123.jpg', 'pakt een eend uit de vijver en stop hem in de frituur'),
 		(2, 'eenden souflé', '123.jpg', 'pakt een eend uit de vijver en gooit hem in de pan')
-
-insert into ForumPost([user_id], postname,post,[date])
+		*/
+insert into ForumPost([user_id], postname, post,[date])
 values	(1, 'eenden zijn lekker', 'If you know what i mean', '2019-8-4'),
 		(2, 'vote kick hugo', 'beetje ongepast', '2019-8-4')
 
